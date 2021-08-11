ef2f7e75f7da075ccf6f4f18f34fce606cefad2b
profiterol <erollooper@gmail.com>
2021-08-11 14:45:32 +0800

    added mixins

--- Complex UX Template/app.js
+++ Complex UX Template/app.js
@@ -1,5 +1,12 @@
-// const toggle = document.querySelector(".switch");
-
-// toggle.addEventListener("checked", function () {
-//   document.body.classList.toggle("dark-theme");
-// });
+$("#darkModeToggle").click(function(){
+  if($('#darkModeToggle').is(':checked')){
+    
+    // $("tasks").addClass("dark-mode");
+    // $("bottom-aside").addClass("dark-mode");
+    // $("body").addClass("dark-mode");
+    $('body').css('background-color', 'black');
+  } else{
+    $("#tasks").removeClass("dark-mode");
+    $("body").removeClass("dark-mode");
+  }
+});

068bc25eff8012fdfb8ef5118dc3e9a91c03d6a7
profiterol <erollooper@gmail.com>
2021-08-18 13:13:55 +0800

    adding extra scss

--- Complex UX Template/app.js
+++ Complex UX Template/app.js
@@ -1,12 +1,12 @@
 $("#darkModeToggle").click(function(){
   if($('#darkModeToggle').is(':checked')){
-    
-    // $("tasks").addClass("dark-mode");
-    // $("bottom-aside").addClass("dark-mode");
-    // $("body").addClass("dark-mode");
     $('body').css('background-color', 'black');
-  } else{
-    $("#tasks").removeClass("dark-mode");
-    $("body").removeClass("dark-mode");
+  } else if(!$('#darkModeToggle').is(':checked')){
+    $('body').css('background-color', 'chocolate');
   }
 });
+
+function toggleTheme(time) {
+  var sheets= document.getElementsByTagName('link');
+      return sheets[0].href = 'css/'+ time+ '.css'
+}

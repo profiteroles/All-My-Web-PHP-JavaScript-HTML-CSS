fc0d8fc891c65f6818f9961a03032dfc5cdbe31b
profiterol <erollooper@gmail.com>
2021-09-24 23:51:47 +0800

    Working on Gallery

--- george-garden/js/app.js
+++ george-garden/js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+let cartCounterDisplay = document.querySelector('#cart-counter');
+
+cartCounterDisplay.innerHTML = `&nbsp;${cartCounter} - $ ${cartPrice}`;
+
+$(function() {
+  $('.searchbar')
+    .bind('click', function(event) {
+      $(".search-field").toggleClass("expand-search");
+      $('.searchbar').toggleClass("active");
+    })
+});
+
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
+
